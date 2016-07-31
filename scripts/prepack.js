0dea064e98505ba5f1b70450ccf1b1ce16fdde4d
Darren Reid <darren.j.reid@gmail.com>
2016-07-31 16:07:54 +1000

    Add typings json for registry
    Remove manual fetch d.ts. Now will require TS users to include typings
    to dt~isomorphic-fetch to work..
    Hack can cause a lot of problems for users also including whatwg-fetch
    or isomorphic-fetch to use another library.

--- scripts/prepack.js
+++ scripts/prepack.js
@@ -10,7 +10,4 @@
 
     fs.rename('./src/index.js','./lib/index.js',renameComplete);
     fs.rename('./src/index.js.map','./lib/index.js.map',renameComplete);
-
-    //HACK to get get downstream dependencies included in npm package and keep VS/TS happy, specifically when using lib from JSPM
-    fs.createReadStream('./typings/globals/isomorphic-fetch/index.d.ts').pipe(fs.createWriteStream('./lib/isomorphic-fetch.d.ts'));
 })();
