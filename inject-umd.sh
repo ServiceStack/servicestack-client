56638e6f839764ce8fdbd787ddbf35dd5a0e0754
Demis Bellot <demis.bellot@gmail.com>
2023-07-13 12:31:50 +0800

    make npm scripts x-plat by using shx

--- inject-umd.sh
+++ inject-umd.sh
@@ -1,1 +1,1 @@
-sed -i 's/})(function/    else if (typeof window != "undefined") factory(window.require||function(){}, window["\@servicestack\/client"]={});\n})(function/' dist/servicestack-client.umd.js
+sed -i '' 's/})(function/    else if (typeof window != "undefined") factory(window.require||function(){}, window["\@servicestack\/client"]={});\n})(function/' dist/servicestack-client.umd.js
