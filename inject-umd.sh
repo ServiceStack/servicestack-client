bb788fc0910424888f2aa9a74f43e38622e61034
Demis Bellot <demis.bellot@gmail.com>
2023-07-13 13:29:23 +0800

    remove non portable usage of sed -i with writing to tmp file

--- inject-umd.sh
+++ inject-umd.sh
@@ -1,1 +1,1 @@
-sed -i '' 's/})(function/    else if (typeof window != "undefined") factory(window.require||function(){}, window["\@servicestack\/client"]={});\n})(function/' dist/servicestack-client.umd.js
+sed 's/})(function/    else if (typeof window != "undefined") factory(window.require||function(){}, window["\@servicestack\/client"]={});\n})(function/' dist/servicestack-client.umd.js > dist/servicestack-client.umd.js.tmp && mv dist/servicestack-client.umd.js.tmp dist/servicestack-client.umd.js
